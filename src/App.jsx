92daf6a655b7c8c367d22cfb52351eb461fef4b6
Nikhil Mahato <nikhilmahato104@gmail.com>
2025-06-18 09:46:51 +0530

    cretaeing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-//WHAT IS USEEFFET?
-//WHAT IS USESTATE did?
-
-//WHAT IS USEEFFECT? useEffect is a React Hook that lets you perform side effects in function components.
-// It is called after the component renders and can be used for tasks like data fetching, subscriptions, or manually changing the DOM.
-//WHAT IS USESTATE? useState is a React Hook that lets you add state to function components.
-// It returns an array with two elements: the current state value and a function to update that state value.
-
-
-
-//WHAT IS THE DIFFERENCE BETWEEN USESTATE AND USEEFFECT? useState is used to manage state in a component, while useEffect is used to perform side effects based on changes in state or props.
-
-
+// import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
 function App() {
-  const [count, setCount] = useState(10)
-
   return (
-    <>
-     
+    <Routes>
+      <Route path="/" element={<Home />} />
       
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       
-      </div>
-    
-    </>
-  )
+      {/* <Route path="/about" element={<About />} /> */}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
